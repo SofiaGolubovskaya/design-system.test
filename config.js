6f9c4e243b2004a9a0ae2020ce5be4c51aca2eb7
Sofia Golubovskaa <sgoly353@gmail.com>
2026-01-22 18:06:07 +0700

    Компоновка стилей

--- config.js
+++ config.js
@@ -4,16 +4,17 @@ const sd = new StyleDictionary({
   source: ['src/shared/styles/tokens/tokens.json'],
   platforms: {
     scss: {
-      transformGroup: 'scss',
+      // transformGroup 'scss' по умолчанию делает длинные имена. 
+      // Мы можем это переопределить через индивидуальные трансформы.
+      transforms: ['attribute/cti', 'name/cti/kebab', 'size/rem', 'color/css'],
       buildPath: 'src/shared/styles/generated/',
-      // Добавляем опции для файлов
       files: [
         {
           destination: '_tokens.scss',
           format: 'scss/variables',
           options: {
-            // Это заставит Style Dictionary не учитывать названия папок/сетов
-            showFileHeader: false 
+            // Эта опция часто помогает убрать лишние уровни, если структура плоская
+            outputReferences: true 
           }
         },
       ],
